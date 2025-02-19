f62d75df999f69400ccb754f3efdd8b854efb89f
YuriiKomaniak <sutem@ukr.net>
2025-02-19 15:38:26 +0200

    Update task_05.js

--- js-tasks/task_05.js
+++ js-tasks/task_05.js
@@ -1,16 +1,7 @@
 /*------Keep Hydrated------*/
 
-//Yurii Komaniak
-function litres(time) {
-  return (Math.trunc(time / 2))
-}
 
-//Oleksandr Krasylnykov
 function litres(time) {
   return (time - time % 2) / 2;
 }
 
-//Nikita Zemlianskyi
-function litres(time) {
-  return Number(time) * 0.5 - ((Number(time) * 0.5) % 1);
-}
