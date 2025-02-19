c7c9dd9d125ee49bacab188b3c05312014c96763
YuriiKomaniak <sutem@ukr.net>
2025-02-19 15:35:31 +0200

    Update task_01.js

--- js-tasks/task_01.js
+++ js-tasks/task_01.js
@@ -1,18 +1,7 @@
 /*------Messi Goals------*/
 
-//Yurii Komaniak
+
 function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
     return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
 }
 
-//Oleksandr Krasylnykov
-function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
-    return Number(laLigaGoals) + Number(copaDelReyGoals) + Number(championsLeagueGoals);
-}
-
-//Nikita Zemlianskyi
-function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
-    return (
-        Number(laLigaGoals) + Number(copaDelReyGoals) + Number(championsLeagueGoals)
-    );
-}
