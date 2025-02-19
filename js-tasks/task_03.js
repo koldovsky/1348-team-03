ae752169dbb3a398b2fd7669d7a4b0ba9ab59e22
YuriiKomaniak <sutem@ukr.net>
2025-02-19 15:36:49 +0200

    Update task_03.js

--- js-tasks/task_03.js
+++ js-tasks/task_03.js
@@ -1,16 +1,7 @@
 /*------Game Move------*/
 
-//Yurii Komaniak
+
 function move(position, roll) {
     return position + roll * 2
 }
 
-//Oleksandr Krasylnykov
-function move(position, roll) {
-    return position = position + roll * 2;
-}
-
-//Nikita Zemlianskyi
-function move(position, roll) {
-    return Number(position) + 2 * Number(roll);
-}
