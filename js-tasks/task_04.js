5b1fd6f367ed42a44e62cbad6b441fb0c0eefef9
YuriiKomaniak <sutem@ukr.net>
2025-02-19 15:37:26 +0200

    Update task_04.js

--- js-tasks/task_04.js
+++ js-tasks/task_04.js
@@ -1,21 +1,7 @@
 /*------Personalized Message------*/
 
-//Yurii Komaniak
-function greet(name, owner) {
-    if (name === owner) {
-        return 'Hello boss';
-    }
-    else {
-        return 'Hello guest';
-    }
-}
-//Oleksandr Krasylnykov
+
 function greet(name, owner) {
     return (name === owner) ? 'Hello boss' : 'Hello guest';
 }
 
-//Nikita Zemlianskyi
-function greet(name, owner) {
-    if (name === owner) return "Hello boss";
-    else return "Hello guest";
-}
