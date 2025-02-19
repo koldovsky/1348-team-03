5c9bfacb216fc8c94ccdaf6ea32b55eebf6e4dda
YuriiKomaniak <sutem@ukr.net>
2025-02-19 15:39:24 +0200

    Update task_06.js

--- js-tasks/task_06.js
+++ js-tasks/task_06.js
@@ -1,17 +1,8 @@
 /*------Opposites Attract------*/
 
-//Yurii Komaniak
+
 function lovefunc(flower1, flower2) {
-    // moment of truth
     return (flower1 + flower2) % 2 === 1;
 }
 
-//Oleksandr Krasylnykov
-function lovefunc(flower1, flower2) {
-    return (flower1 % 2 === 0 && flower2 % 2 === 1) || (flower1 % 2 === 1 && flower2 % 2 === 0)
-}
 
-//Nikita Zemlianskyi
-function lovefunc(flower1, flower2) {
-    return (Number(flower1) + Number(flower2)) % 2 === 1;
-}
