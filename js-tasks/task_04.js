d519b179e1853be39f2f55c6f3a958aacc951876
YuriiKomaniak <sutem@ukr.net>
2025-02-20 14:58:44 +0200

    Update task_04.js

--- js-tasks/task_04.js
+++ js-tasks/task_04.js
@@ -1,6 +1,17 @@
-/*------Personalized Message------*/
+/*   https://www.codewars.com/kata/544a54fd18b8e06d240005c0/train/javascript     */
+/* -----  Smallest value of an array ------*/
 
+/*--------Yurii Komaniak---------*/
 
+function min(arr, toReturn) { 
+  let minNumber=arr[0] ;
+  let index=0;
+  for (let i=1;i<arr.length;i++) {
+     if (minNumber>arr[i]){minNumber=arr[i]; index=i;}
+   }
+  return toReturn==="value"?minNumber:index;
+  
+}
 function greet(name, owner) {
     return (name === owner) ? 'Hello boss' : 'Hello guest';
 }
