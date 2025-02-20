9005fc738fa1529e60532c0d362ee4ad609dc7f6
YuriiKomaniak <sutem@ukr.net>
2025-02-20 18:29:10 +0200

    Create task_05.js

---
+++ js-tasks/tasks-05/task_05.js
@@ -0,0 +1,8 @@
+/*       https://www.codewars.com/kata/53ee5429ba190077850011d4/train/javascript      */
+
+/*--------------------You Can't Code Under Pressure #1--------------------------*/
+
+/*------------------Yurii Komaniak----------------------*/
+function doubleInteger(i) {
+  return i*2;
+}
