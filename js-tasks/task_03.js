b62e309a8e94a6e5eb35c247cb7077507ea30580
YuriiKomaniak <sutem@ukr.net>
2025-02-20 14:41:47 +0200

    Update task_03.js

--- js-tasks/task_03.js
+++ js-tasks/task_03.js
@@ -1,7 +1,21 @@
-/*------Game Move------*/
+/*  https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript   */
+/*--------Find Maximum and Minimum Values of a List---------*/
 
+/*--------Yurii Komaniak----------*/
 
-function move(position, roll) {
-    return position + roll * 2
+var min = function(list){
+   minNumber=list[0] ;
+   for (i=1;i<list.length;i++) {
+     if (minNumber>list[i]){minNumber=list[i];}
+   }
+   return minNumber;
+}
+
+var max = function(list){
+    maxNumber=list[0] ;
+   for (i=1;i<list.length;i++) {
+     if (maxNumber<list[i]){maxNumber=list[i];}
+   }
+   return maxNumber;
 }
 
