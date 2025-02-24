c228c84b37e23098c80518639d3feeb9ec072d54
AlxKrs <98707821+AlxKrs@users.noreply.github.com>
2025-02-24 15:26:56 +0200

    Update task_04.js

--- js-tasks/task_04.js
+++ js-tasks/task_04.js
@@ -27,3 +27,31 @@ class Dog extends Animal {
   }
 }
 
+
+/*------- Oleksandr Krasylnykov ------*/
+class Shark extends Animal {
+  constructor(name, age, status) {
+    super(name, age, 0, "shark", status);
+  }
+}
+
+class Cat extends Animal {
+  constructor(name, age, status, legs, species) {
+    super(name, age, 4, "cat", status);
+  }
+  
+  introduce() {
+    return super.introduce() + "  Meow meow!";
+  }
+}
+
+class Dog extends Animal {
+  constructor(name, age, status, master, legs, species) {
+    super(name, age, 4, "dog", status);
+    this.master = master;
+  }
+  
+  greetMaster() {
+    return `Hello ${this.master}`;
+  }
+}
