d47df136898df5da734740bba4e3550b8920097a
Viacheslav1403 <viacheslavofficial2@gmail.com>
2025-02-27 23:11:25 +0000

    day-one-changes

--- js/single-tour.day-one.js
+++ js/single-tour.day-one.js
@@ -1,36 +1,36 @@
-document.addEventListener("DOMContentLoaded"),
-  () => {
-    console.log("JS Loaded!");
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("JS Loaded!");
 
-    const section = document.querySelector(".single-tour-day-one");
-    const content = document.querySelector(".single-tour-day-one__content");
-    const gallery = document.querySelector(".single-tour-day-one__gallery");
+  const container = document.querySelector(".single-tour-day-one__container");
+  const content = document.querySelector(".single-tour-day-one__content");
+  const gallery = document.querySelector(".single-tour-day-one__gallery");
 
-    if (!section || !content || !gallery) {
-      console.error("❌ Один із блоків не знайдено!");
-      return;
-    }
-  };
-
-function updateLayout() {
-  if (window.innerWidth > 1024) {
-    section.style.display = "flex";
-    section.style.flexDirection = "row";
-    section.style.alignItems = "flex-start";
-
-    content.style.flex = "1";
-    content.style.maxWidth = "50%";
-
-    gallery.style.flex = "1";
-    gallery.style.maxWidth = "50%";
-  } else {
-    section.style.display = "flex";
-    section.style.flexDirection = "column";
-    content.style.maxWidth = "100%";
-    gallery.style.maxWidth = "100%";
+  if (!container || !content || !gallery) {
+    console.error("❌ Один із блоків не знайдено!");
+    return;
   }
-}
 
-updateLayout();
+  function updateLayout() {
+    if (window.innerWidth > 768) {
+      container.style.display = "flex";
+      container.style.flexDirection = "row";
+      container.style.alignItems = "flex-start";
 
-window.addEventListener("resize", updateLayout);
+      content.style.flex = "1";
+      content.style.maxWidth = "50%";
+
+      gallery.style.flex = "1";
+      gallery.style.maxWidth = "50%";
+    } else {
+
+      container.style.display = "flex";
+      container.style.flexDirection = "column";
+
+      content.style.maxWidth = "100%";
+      gallery.style.maxWidth = "100%";
+    }
+  }
+
+  updateLayout();
+  window.addEventListener("resize", updateLayout);
+});
