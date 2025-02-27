0c44e19c7974389560a445e1ebc611b26d05f3b1
Viacheslav1403 <viacheslavofficial2@gmail.com>
2025-02-27 21:29:08 +0000

    single-tour-js

---
+++ js/single-tour.js
@@ -0,0 +1,14 @@
+function init() {
+  import("./single-tour.discount.partial.js");
+  import("./single-tour.photos.js");
+}
+
+const totalPartials = document.querySelectorAll(
+  '[hx-trigger="load"], [data-hx-trigger="load"]'
+).length;
+let loadedPartialsCount = 0;
+
+document.body.addEventListener("htmx:afterOnLoad", () => {
+  loadedPartialsCount++;
+  if (loadedPartialsCount === totalPartials) init();
+});
